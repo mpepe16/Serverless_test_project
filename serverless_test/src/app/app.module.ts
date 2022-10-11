55f450f82e7a4576909b0a5fdf171bc8af60d734
mpepe16 <mpepe66@gmail.com>
2022-10-11 13:26:52 +0200

    Add: CoreUI grid and button.

--- serverless_test/src/app/app.module.ts
+++ serverless_test/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { TestButtonComponent } from './components/test-button/test-button.component';
+import { TextAreaComponent } from './components/text-area/text-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestButtonComponent,
+    TextAreaComponent,
   ],
   imports: [
     BrowserModule

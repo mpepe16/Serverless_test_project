5729581b5aa12e309aff268c38ffb1190b240709
mpepe16 <mpepe66@gmail.com>
2022-10-14 21:36:37 +0200

    The rest are mostly auto generated and probably some dead code.

---
+++ Backend/ServerlessApp/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { HelloServiceService } from './hello-service.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ServerlessApp';
+  newData:any;
+  constructor (private  _apiservice: HelloServiceService) {
+
+  }
+  getHello() {
+    this._apiservice.getData().subscribe(data => {
+      this.newData = data;
+    })
+  }
+}
---
+++ Backend/ServerlessApp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ButtonModule } from '@coreui/angular';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ButtonModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

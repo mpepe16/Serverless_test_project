45e292d9224b0f7813c55f92c359877ecbc1ef34
mpepe16 <mpepe66@gmail.com>
2022-10-14 21:32:36 +0200

    Lambda function generated by sam.

---
+++ Backend/Hello-sam/hello-world/app.ts
@@ -0,0 +1,38 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ *
+ */
+
+export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    let response: APIGatewayProxyResult;
+    try {
+        response = {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Headers" : "Content-Type",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "POST,GET"
+            },
+            body: JSON.stringify({
+                message: 'Not sure when was the last to be so happy so see the sentence: hellow world',
+            }),
+        };
+    } catch (err: unknown) {
+        console.log(err);
+        response = {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: err instanceof Error ? err.message : 'some error happened',
+            }),
+        };
+    }
+
+    return response;
+};
